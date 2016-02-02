32adbf0b3cdda1756e57022e7b8f3876ee514745
BattleJohnFantastica <johnwlmurphy@gmail.com>
2016-02-02 23:15:11 +0000

    Quotes

--- bot.js
+++ bot.js
@@ -12,6 +12,7 @@ function respond() {
       botRegex5 = /^\/John$/;
       botRegex6 = /^\/reddit$/;
       botRegex7 = /^\/wow$/;
+      botRegex8 = /^\/quotes$/;
 
   if(request.text && botRegex1.test(request.text)) {
     this.res.writeHead(200);
@@ -75,6 +76,16 @@ function respond() {
     if(request.text && botRegex7.test(request.text)) {
     this.res.writeHead(200);
     postMessage7();
+    this.res.end();
+      } else {
+    console.log("don't care");
+    this.res.writeHead(200);
+    this.res.end();
+  }
+    
+    if(request.text && botRegex8.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage8();
     this.res.end();
       } else {
     console.log("don't care");
@@ -371,4 +382,56 @@ var keyword = keywords[Math.floor(Math.random()*keywords.length)];
   });
   botReq.end(JSON.stringify(body));
 }
+
+function postMessage8() {
+  
+  var botResponse, keywords, options, body, botReq;
+    
+    keywords = [
+ ">Le bruit de la fin du monde dans tes oreilles", 
+ ">All that is gold does not glitter", 
+ ">To know both sides of a quarrel, is to judge without hate or alarm", 
+ ">J'arrive pas à voir les choses qui font le vrai bonheur je dois être aveugle", 
+ ">Gedanken ohne Inhalt sind leer, Anschauungen ohne Begriffe sind blind", 
+ ">I can hear it screaming in my mind", 
+ ">meh is how I conclude all proofs instead of with the Halmos box",
+ ">Subjects cannot be dissolved into nature, for in that case what gives nature its sense would be missing",
+ ">You know Jeeves, I feel I could do better justice to this philosophy if I understood what all these words meant",
+ ">It is a monster Begot upon itself, born on itself",
+ ">I only got 10 likes in the last 5 minutes, do you think I should take it down? Let me take another selfie",
+ ">being me has its advantages"
+];
+var keyword = keywords[Math.floor(Math.random()*keywords.length)];
+    
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : keyword,
+    
+       };
+
+  console.log('sending ' + keyword + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
 exports.respond = respond;
